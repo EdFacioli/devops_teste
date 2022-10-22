8d1adc17a4cc0268a25c6131685e9e9e727bd0de
Edson Facioli <edfacioli@gmail.com>
2022-10-22 17:19:08 -0300

    Remove useless comment

--- terraform-infra/modules/ecr/main.tf
+++ terraform-infra/modules/ecr/main.tf
@@ -2,32 +2,6 @@ resource "aws_ecr_repository" "main" {
   name = var.repository_name
 }
 
-# resource "aws_ecr_repository_policy" "main" {
-#   repository = aws_ecr_repository.main.name
-#   policy     = <<EOF
-#   {
-#     "Version": "2008-10-17",
-#     "Statement": [
-#       {
-#         "Sid": "adds full ecr access to the test repository",
-#         "Effect": "Allow",
-#         "Principal": "*",
-#         "Action": [
-#           "ecr:BatchCheckLayerAvailability",
-#           "ecr:BatchGetImage",
-#           "ecr:CompleteLayerUpload",
-#           "ecr:GetDownloadUrlForLayer",
-#           "ecr:GetLifecyclePolicy",
-#           "ecr:InitiateLayerUpload",
-#           "ecr:PutImage",
-#           "ecr:UploadLayerPart"
-#         ]
-#       }
-#     ]
-#   }
-#   EOF
-# }
-
 resource "aws_ecr_lifecycle_policy" "name" {
   repository = aws_ecr_repository.main.name
   policy = <<EOF
